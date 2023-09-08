84cdff51ba2adc61a36b18c2be67424696032e32
RodriguezRob <122117829+RodriguezRob@users.noreply.github.com>
2023-09-08 13:05:20 -0400

    adding content to the load up page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/pages/Home";
 import Services from './components/pages/Services';
 import Products from "./components/pages/Products";
 import SignUp from "./components/pages/SignUp";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -20,6 +21,8 @@ function App() {
           <Route path="/sign-up" Component={SignUp} />
         </Routes>
       </Router>
+      <Home />
+      <Footer />
     </div>
   );
 }
